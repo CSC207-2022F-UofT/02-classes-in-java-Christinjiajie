f5bb4129e268110c3db517db8ec61a0c4542b1a7
Christinjiajie <chrsitinjiajie.zhu@mail.utoronto.ca>
2022-10-09 20:43:59 -0400

    a2

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -119,7 +119,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0,1,2,3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -165,6 +165,7 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+        return this.name.equals(other.name);
     }
 
     /**
